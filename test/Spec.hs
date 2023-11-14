91b014f566bc604834fde4ae17cbc64e58f597f6
Anton Medvedev <anton@medv.io>
2023-11-14 18:21:31 +0100

    Split test into files

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,42 +1,20 @@
-import Lib
-import Test.Tasty
-import Test.Tasty.HUnit
+module Main where
 
+import Test.HUnit
+import qualified CardTests as CT
+import qualified DeckTests as DT
+import qualified SshTests as ST
+import qualified AuthTests as AT
+import qualified HandleTests as HT
+import qualified SecTests as SCT
+
+-- Main function to run all tests
 main :: IO ()
-main = defaultMain test
+main = do
+    putStrLn "Running tests..."
+    _ <- runTestTT allTests
+    return ()
 
-test :: TestTree
-test = testGroup "Tests" [maintests]
-
-maintests :: TestTree
-maintests = testGroup "unit tests" [uEq, uShow, uIsOrigin, uDistance]
-
-uEq :: TestTree
-uEq =
-  testGroup
-    "eq"
-    [ testCase "same x and y" $ (Point 2 3 == Point 2 3) @?= True,
-      testCase "same x, different y" $ (Point 2 3 == Point 2 5) @?= False,
-      testCase "same y, different x" $ (Point 2 3 == Point 4 3) @?= False,
-      testCase "different x and y" $ (Point 2 3 == Point 4 5) @?= False
-    ]
-
-uShow :: TestTree
-uShow =
-  testGroup "show" [testCase "some point" $ show (Point 2 3) @?= "(2, 3)"]
-
-uIsOrigin :: TestTree
-uIsOrigin =
-  testGroup
-    "isOrigin"
-    [ testCase "at origin" $ isOrigin (Point 0 0) @?= True,
-      testCase "not at origin, x not 0" $ isOrigin (Point 1 0) @?= False,
-      testCase "not at origin, y not 0" $ isOrigin (Point 0 1) @?= False,
-      testCase "not at origin, x and y not 0" $ isOrigin (Point 2 3) @?= False
-    ]
-
-uDistance :: TestTree
-uDistance =
-  testGroup
-    "distance"
-    [testCase "same location" $ distance (Point 2 3) (Point 2 3) @?= 0.0]
+-- Combine all test cases
+allTests :: Test
+allTests = TestList [CT.cardTests, DT.deckTests, ST.sshTests, AT.authTests, HT.handleTests, SCT.secTests]
